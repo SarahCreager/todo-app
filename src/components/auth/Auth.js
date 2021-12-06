57fcdb59bf3929ce3cd4836745db1f2c94b0025d
Sarah Creager <sarah.f.jamieson@gmail.com>
2021-12-05 23:13:26 -0800

    adds login component

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import {When} from 'react-if';
+import { When } from 'react-if';
 
 import { AuthContext } from '../../context/auth.js';
 
 function Login(props) {
 
   let auth = useContext(AuthContext);
+  
+  const isLoggedIn = auth.isLoggedIn;
+  const authorized = props.capability ? auth.isAuthorized(props.capability) : true;
+  const okToRender = isLoggedIn && authorized;
 
-  render() {
-
-    const isLoggedIn = auth.isLoggedIn;
-    const authorized = props.capability ? auth.isAuthorized(props.capability) : true;
-    const okToRender = isLoggedIn && authorized;
-
-    return (
-      <When condition={okToRender}>
-        {props.children}
-      </When>
-    );
-  }
+  return (
+    <When condition={okToRender}>
+      {props.children}
+    </When>
+  );
 }
 
 export default Login;
